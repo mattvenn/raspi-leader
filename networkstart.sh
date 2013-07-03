4742ef87a6ec28ba547347bcf4604c55006e5f19
Matt Venn <matt@mattvenn.net>
2013-07-03 16:26:29 +0100

    use sudo for ifconfig, and syslog logging.

--- networkstart.sh
+++ networkstart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 interface=eth1
 echo bringing up $interface
-ifconfig $interface 192.168.1.1
+sudo ifconfig $interface 192.168.1.1
 echo NAT
 sudo /sbin/iptables --table nat -A POSTROUTING -o wlan0 -j MASQUERADE
 echo dchp server in background
-sudo udhcpd udhcpd.conf
+sudo udhcpd udhcpd.conf -S
--- refreshpi.sh
+++ refreshpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cmd='parallel-ssh -ih pis.list'
-echo "rm all files"
-$cmd rm -rf *
+echo "rm all raspi-workshop"
+$cmd rm -rf raspi-workshop
 echo "clone git repo"
 $cmd git clone git@github.com:mattvenn/raspi-workshop
 echo "checking files"
