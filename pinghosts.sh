65a4dcc4e0db98781b47b78821b86be675a0a79a
Matt Venn <matt@mattvenn.net>
2013-07-12 18:35:45 +0100

    updated for pis.list

--- pinghosts.sh
+++ pinghosts.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 while true; do
-    for i in $(seq 5 ); do
-        ping -qc 1 raspberrypi$i.local
+    for pi in $(cat pis.list | sed -e 's/pi@//'); do
+        ping -qc 1 $pi
     done
+    echo wait ===================
     sleep 5 
 done
