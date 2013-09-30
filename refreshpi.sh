1d6307a0a1e7dcc1dea385baf2e0fa2e982f6296
Matt Venn <matt@mattvenn.net>
2013-09-30 19:32:15 +0100

    turn on all outputs

--- refreshpi.sh
+++ refreshpi.sh
@@ -6,3 +6,5 @@ echo "clone git repo"
 $cmd git clone https://github.com/mattvenn/raspi-workshop.git
 echo "checking files"
 $cmd ls
+echo "turning on all outputs"
+$cmd sudo python /home/pi/raspi-workshop/flash_led/all_on.py
