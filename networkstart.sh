2085a136501a73d542d087d94f49db3e2d4d2b9a
Matt Venn <matt@mattvenn.net>
2013-10-14 16:09:10 +0100

    added forwarding to network start

--- networkstart.sh
+++ networkstart.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-interface=eth1
+interface=eth3
 echo bringing up $interface
 sudo ifconfig $interface 192.168.2.1
 echo NAT
+sudo sh -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'
 sudo /sbin/iptables --table nat -A POSTROUTING -o wlan0 -j MASQUERADE
 echo dchp server in background
 sudo udhcpd udhcpd.conf -S
