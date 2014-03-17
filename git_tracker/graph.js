19b5d3d59c78053678df2af7bac24f73f537c128
Matt Venn <matt@mattvenn.net>
2014-03-17 16:52:24 +0000

    removed unwanted add

--- git_tracker/graph.js
+++ git_tracker/graph.js
@@ -4,3 +4,17 @@
             {lines: {show: true}, points: {show:true}, color: 'blue', data: $line_data, yaxis:2}
         ]
         ,options);
+
+        $("#placeholder-$num").bind("plothover", function (event, pos, item) {
+
+				if (item) {
+					var x = item.datapoint[0].toFixed(2),
+						y = item.datapoint[1].toFixed(2);
+
+					$("#tooltip").html(y + " lines")
+						.css({top: item.pageY+5, left: item.pageX+5})
+						.fadeIn(200);
+				} else {
+					$("#tooltip").hide();
+				}
+		});
--- git_tracker/pi_work/192.168.0.12/matt/matt.py
+++
@@ -1,2 +0,0 @@
-#my first prog
-import turtle
