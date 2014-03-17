e747ef0801c119bc05f5a339b7647116fad2c63b
Matt Venn <matt@mattvenn.net>
2014-03-17 03:41:45 +0000

    added more shared vars

--- git_tracker/config.py
+++ git_tracker/config.py
@@ -1,6 +1,14 @@
+#config
 
+verbose = True
 #time_res = 1000 * 60 * 60 * 24 # days
 time_res = 1000 * 60 # minutes
-host_ips = [ '192.168.0.12' ]
-#local_dir = '/home/matthew/work/cpdworkshops/raspi/raspi-workshop'
+#where the local git repo is
 local_dir = './pi_work'
+#only want these files
+file_extension='*py'
+
+#variable for student's computers
+host_ips = [ '192.168.0.12' ]
+user_name = 'pi'
+work_dir = '~/work'
