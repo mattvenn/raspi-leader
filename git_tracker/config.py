c63af8b8727066a4d9a7577a9f2eebda395c4e1e
Matt Venn <matt@mattvenn.net>
2014-03-17 16:51:39 +0000

    last hour only

--- git_tracker/config.py
+++ git_tracker/config.py
@@ -1,16 +1,19 @@
-#config
+#config file
 import time
 
 verbose = True
-time_res = 1000 * 60 * 60 #hours
-min_time = time.time() * 1000 - time_res * 24
-#time_res = 1000 * 60 # minutes
-#where the local git repo is
+
+#time_res is used for calculating syntax error bar widths
+#time_res = 1000 * 60 * 60 #hours
+time_res = 1000 * 60 #mins
+min_time = time.time() * 1000 - time_res * 60
+
+#where the local git repo will be created
 local_dir = './pi_work'
-#only want these files
+#only these files will be added
 file_extension='*py'
 
-#variable for student's computers
+#variables for student's computers
 host_ips = [ '192.168.0.12' ]
 user_name = 'pi'
 work_dir = '~/work'
